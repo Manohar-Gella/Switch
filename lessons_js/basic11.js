1ab906af72dfe0280d18e2259b3b0bc939f5d321
Manohar-Gella <manohargella272@gmail.com>
2023-12-16 21:20:51 +0530

    function exe 2 setTimeout

---
+++ lessons_js/basic11.js
@@ -0,0 +1,9 @@
+// function exe 2 using setTimeout
+function greetdad() {
+    console.log("Hello daddy");
+}
+function greetmom() {
+    console.log("Hello mom");
+}
+
+setTimeout(greetdad, 3*1000);
