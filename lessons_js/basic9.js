6342a06f3aabfabcfd2989f853f9af70f00f36ac
Manohar-Gella <manohargella272@gmail.com>
2023-12-16 20:32:29 +0530

    functions

---
+++ lessons_js/basic9.js
@@ -0,0 +1,18 @@
+// functions in javascript
+
+function addision(a, b){
+    return a+b;
+}
+
+let sol = addision(3, 3);
+console.log(sol);
+
+// exple code for function
+
+function wish(fname, lname){
+    let out = "hello "+fname + " "+lname;
+    return out;
+}
+
+let term = wish("Manohar", "Gella");
+console.log(term);
