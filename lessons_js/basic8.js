7f58f34282d37759002d413edffaf1a9737a2a73
Manohar-Gella <manohargella272@gmail.com>
2023-12-16 20:27:17 +0530

    object's

---
+++ lessons_js/basic8.js
@@ -0,0 +1,30 @@
+// objects in javascript
+
+const users = [{
+    firstname: "Manohar",
+    lastname: "Gella",
+    gender: "male",
+    age: 18
+},{
+    firstname: "Pavithra",
+    lastname: "Gella",
+    gender: "female",
+    age: 15
+},{
+    firstname: "Neriyya",
+    lastname: "Gella",
+    gender: "male",
+    age: 45
+},{
+    firstname: "Nirmala",
+    lastname: "Gella",
+    gender: "female",
+    age: 42
+
+}]
+
+for(let i = 0; i < users.length; i++){
+    if(users[i].gender == "female"){
+        console.log(users[i]);
+    }
+}
