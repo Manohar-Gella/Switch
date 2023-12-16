ba4d58ea5d2a52dac8ef3d8bcdeba2c50b0a420f
Manohar-Gella <manohargella272@gmail.com>
2023-12-16 19:58:23 +0530

    array exe 2

---
+++ lessons_js/basic6.js
@@ -0,0 +1,10 @@
+// advanced arrays
+
+let names = ["Manohar Gella", "Nirmala Gella", "Neriyya Gella", "Pavithra Gella"];
+let gender = ["male", "female", "male", "female"];
+
+for(let i = 0; i < names.length; i++){
+    if(gender[i] == "male"){
+        console.log(names[i])
+    }
+}
