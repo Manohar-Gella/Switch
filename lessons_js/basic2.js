bf4ff57df44c7d9f3fda1c864c4ff72c5e0d4a90
Manohar-Gella <manohargella272@gmail.com>
2023-12-16 19:42:18 +0530

    if statements

--- lessons_js/basic2.js
+++ lessons_js/basic2.js
@@ -1,2 +1,10 @@
-let married = false;
-console.log(married)
+// if statements in Javascript
+
+let status = true;
+
+if(status == true){
+    console.log("Oh ok");
+}
+else{
+    console.log("can i have you");
+}
