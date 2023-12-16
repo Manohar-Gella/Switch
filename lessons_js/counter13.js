c7921011b6b88b617434ae8541292096ebfa067a
Manohar-Gella <manohargella272@gmail.com>
2023-12-16 21:41:10 +0530

    0-30 sec counter

---
+++ lessons_js/counter13.js
@@ -0,0 +1,19 @@
+// counter 0 -30 seconds in javascript
+function countdown() {
+    let count = 30;
+  
+    const timer = setInterval(() => {
+      console.clear(); // Clears the console on each iteration for a cleaner display
+  
+      if (count >= 0) {
+        console.log(count);
+        count--;
+      } else {
+        clearInterval(timer); // Stop the countdown when it reaches 0
+      }
+    }, 1000); // Update every second
+  }
+  
+  // Start the countdown
+  countdown();
+  
