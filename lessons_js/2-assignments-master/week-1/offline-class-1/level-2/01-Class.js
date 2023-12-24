54d86622f9059f6f8c92d7ae4563c3ab319b9934
Manohar-Gella <manohargella272@gmail.com>
2023-12-25 04:32:03 +0530

    classes

--- lessons_js/2-assignments-master/week-1/offline-class-1/level-2/01-Class.js
+++ lessons_js/2-assignments-master/week-1/offline-class-1/level-2/01-Class.js
@@ -9,4 +9,30 @@ class Animal {
   }
 }
 
+// my exp
+
+
+
+class students {
+	constructor(name, age, grade){
+		this.name = name
+		this.age = age
+		this.grade = grade
+	}
+	who(){
+		console.log("name: "+this.name);
+		console.log("age: "+this.age);
+		console.log("grade: "+this.grade);
+	}
+	wgender(){
+		console.log(this.name + " Geder is " + this.grade)
+	}
+}
+
+let s1 = new students("Manohar Gella", 18, "male");
+let s2 = new students("Neriyya Gella", 50, "male");
+s1.wgender();
+s1.who();
+s2.who();
+s2.wgender();
 
