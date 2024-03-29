6d9773bcacf93a21b7e3dabd783b2a7b56b0e2e8
Manohar Gella <manohargella272@gmail.com>
2024-03-30 00:52:12 +0530

    Js Number Apis

---
+++ 100x/week-1/9-JsNumApi.js
@@ -0,0 +1,16 @@
+// parseInt -> converts str in int
+
+let x = '20';
+let y = '10';
+console.log(`Before pase ${x+y}`);
+console.log(`After pase ${parseInt(x)+parseInt(y)}`);
+
+
+
+// parseFloat -> converts str into float
+
+let z = '69';
+let fz = parseFloat(z)
+console.log(`Before pase ${z}`);
+console.log(`After pase ${parseFloat(z)}`);
+console.log(typeof fz)
