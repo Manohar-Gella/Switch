dfff1c5086f7c50cf133b350c58d22cb641821d4
Manohar Gella <manohargella272@gmail.com>
2024-04-09 00:59:57 +0530

    test

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-setTimeout
+// promises and async await javascript
+
+function counter(till){
+    let sol = new Promise(function(){
+        let sum = 0;
+        for(let i = 0; i<=till; i++){
+            sum+=i;
+        }
+        console.log(sum);
+    });
+   
+}
+
+async function display(){
+    await counter(5);
+}
+
+console.log("Manohar")
+console.log("Gella")
+display()
+console.log("After")
