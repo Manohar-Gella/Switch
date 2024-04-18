b497e6ae2d31fbbec1a9245336f65925253ef4d7
Manohar Gella <manohargella272@gmail.com>
2024-04-19 02:24:11 +0530

    Try catch

---
+++ 100x/week-3/12-try-catch.js
@@ -0,0 +1,15 @@
+
+try {
+    function getLength(str){
+        return str.length
+    }
+    
+    let ans = getLength();
+    
+    console.log(ans);
+    
+}
+
+catch {
+    console.log("Something went wrong");
+}
