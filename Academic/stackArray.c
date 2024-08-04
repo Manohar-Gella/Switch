90875f138ca2f616d5747b80f9a389bd49cc768f
Manohar Gella <manohargella272@gmail.com>
2024-08-04 17:43:36 +0530

    Stack Using Array

--- Academic/stackArray.c
+++ Academic/stackArray.c
@@ -1,62 +1,62 @@
-#include <stdio.h>
+// stack implementation of array
+#include<stdio.h>
 #define SIZE 5
 int stack[SIZE];
 int top = -1;
 
 int display(){
-    if(top == -1){
-        printf("Stack is empty bro\n");
+    for(int i = 0; i<=top; i++){
+        printf("%d ", stack[i]);
     }
-    else{
-            for(int i = 0; i<=top; i++){
-            printf("%d ", stack[i]);
-        }
-    }
-    
 }
 
 int push(){
-    int value;
-    printf("Enter the value to push: ");
-    scanf("%d", &value);
-    top++;
-    stack[top] = value;
+    int temp;
+    printf("\nEnter which element to push: ");
+    scanf("%d", &temp);
+    top ++;
+    stack[top] = temp;
+    printf("Element %d sucecessfully inserted", temp);
 }
 
 int pop(){
-    if(top < 0){
-        printf("Stack is empty\n");
-    }
-    else{
-        printf("value sucessfully removed\n");
-        top --;
-    }
+    printf("last element sucessfully removed");
+    top --;
 }
 
-int main() {
+
+int main(){
     while(1){
         int choice;
-        printf("Enter any choice");
-        printf("\n1.Display\n2.Push\n3.Pop\n:");
+        printf("\n Enter any choice \n");
+        printf("1.Display\n2.Push\n3.Pop\n:");
         scanf("%d", &choice);
         switch(choice){
             case 1:
-                display();
+                if(top != -1){
+                    display();
+                }else{
+                    printf("\nStack is empty to display");
+                }
                 break;
             case 2:
-                if(top >= SIZE - 1){
-                    printf("Stack is full\n");
-                }
-                else{
+                if(top>=SIZE-1){
+                    printf("\nStack is full push elements ");
+                }else{
                     push();
                 }
                 break;
             case 3:
-                pop();
+                if(top == -1){
+                    printf("The stack is empty to pop elements");
+                }else{
+                    pop();
+                }
                 break;
             default:
                 printf("Something went wrong");
-        }
-        
+                break;
     }
+    }
+    return 0;
 }
