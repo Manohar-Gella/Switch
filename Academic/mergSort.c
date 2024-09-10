dd1286ca703f7db6b82c19929949fd63cd64330b
Manohar Gella <manohargella272@gmail.com>
2024-09-11 01:54:37 +0530

    Merge sort

--- Academic/mergSort.c
+++ Academic/mergSort.c
@@ -1,81 +1,61 @@
 #include <stdio.h>
-#include <stdlib.h>
-// Merge two subarrays L and M into arr
-void merge(int arr[], int p, int q, int r) {
-// Create L ← A[p..q] and M ← A[q+1..r]
-int n1 = q - p + 1;
-int n2 = r - q;
-int *L, *M;
-int i,j,k;
-L = malloc(n1 * sizeof(arr[0]));
-M = malloc(n2 * sizeof(arr[0]));
-for (i = 0; i < n1; i++)
-L[i] = arr[p + i];
-for (j = 0; j < n2; j++)
-M[j] = arr[q + 1 + j];
-// Maintain current index of sub-arrays and main array
-i = 0;
-j = 0;
-k = p;
-// Until we reach either end of either L or M, pick larger among
-// elements L and M and place them in the correct position at A[p..r]
-while (i < n1 && j < n2) {
-if (L[i] <= M[j]) {
-arr[k] = L[i];
-i++;
-} else {
-arr[k] = M[j];
-j++;
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while(i < n1 && j < n2) {
+        if(L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while(i < n1) {
+        arr[k++] = L[i++];
+    }
+
+    while(j < n2) {
+        arr[k++] = R[j++];
+    }
 }
-k++;
-}
-// When we run out of elements in either L or M,
-// pick up the remaining elements and put in A[p..r]
-while (i < n1) {
-arr[k] = L[i];
-i++;
-k++;
-}
-while (j < n2) {
-arr[k] = M[j];
-j++;
-k++;
-}
-}
-// Divide the array into two subarrays, sort them and merge them
+
 void mergeSort(int arr[], int l, int r) {
-if (l < r) {
-// m is the point where the array is divided into two subarrays
-int m = l + (r - l) / 2;
-mergeSort(arr, l, m);
-mergeSort(arr, m + 1, r);
-// Merge the sorted subarrays
-merge(arr, l, m, r);
+    if(l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
 }
-}
-// Print the array
+
 void printArray(int arr[], int size) {
-int i;
-for (i = 0; i < size; i++)
-printf("%d ", arr[i]);
-printf("\n");
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
-// Driver program
+
 int main() {
-int n,i,size;
-int *arr;
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    printf("Given array is \n");
+    printArray(arr, arr_size);
 
-printf("enter numberof elements:\n");
-scanf("%d", &n);
-arr = malloc(n * sizeof(*arr));
-for (i=0; i<n; i++)
-{
-printf("enter element %d:\n", i+1);
-scanf("%d", &arr[i]);
-}
-mergeSort(arr, 0, n - 1);
-printf("Sorted array: \n");
-printArray(arr, n);
+    mergeSort(arr, 0, arr_size - 1);
 
-return 0;
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
 }
