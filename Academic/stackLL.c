fbcd6d5410f0ac8bc207da058021146f679ba214
Manohar Gella <manohargella272@gmail.com>
2024-09-11 01:43:13 +0530

    stack using linked list

--- Academic/stackLL.c
+++ Academic/stackLL.c
@@ -8,7 +8,10 @@ struct Node {
 
 struct Node* top = NULL;
 
-void push(int x) {
+void push() {
+    int x;
+    printf("Enter the element to push: ");
+    scanf("%d", &x);
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = x;
     newNode->next = top;
@@ -26,22 +29,38 @@ void pop() {
 }
 
 void display() {
-    struct Node* temp = top;
-    while (temp != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
+    if (top == NULL) {
+        printf("Stack is empty\n");
+    } else {
+        struct Node* temp = top;
+        while (temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
     }
-    printf("\n");
 }
 
 int main() {
-    push(1);
-    push(2);
-    push(3);
-    push(4);
-    push(5);
-    display();
-    pop();
-    display();
+    int choice;
+    while (1) {
+        printf("\n1. Push\n2. Pop\n3. Display\n4. Exit\nEnter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
     return 0;
 }
